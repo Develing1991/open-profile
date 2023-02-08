aa39c807b0d177c65b692097d394ffa61e2edb7a
Develing1991 <completed0728@gmail.com>
2023-02-08 10:42:59 +0900

    메인 텍스트 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -10,8 +10,8 @@ export default function Main() {
 				<em className="text-xs text-orange-300 not-italic tracking-wider font-bold  xl:text-lg lg:text-base md:text-sm sm:text-xs max-sm:p-4  transition-all duration-1000">
 					Wellcome to my...
 				</em>
-				<h2 className="text-3xl text-orange-200 font-semibold tracking-widest xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl max-sm:p-4  transition-all duration-1000 drop-shadow-lg ">
-					Develling1991
+				<h2 className="text-3xl text-orange-200 font-semibold tracking-widest xl:text-9xl lg:text-7xl md:text-6xl sm:text-5xl max-sm:p-4  transition-all duration-1000 drop-shadow-lg hover:scale-110">
+					DeveLLing1991
 				</h2>
 				<div className="flex mt-6 justify-center gap-10 text-2xl max-sm:text-xl transition-all max-sm:gap-5 text-orange-200">
 					<div className="p-2 hover:text-cyan-500 hover:scale-110 hover:cursor-pointer">
